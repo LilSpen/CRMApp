a44ac1c28f48aa2129b8d0eed4256d9e3492d447
LilSpen <116097698+LilSpen@users.noreply.github.com>
2025-03-21 13:52:32 -0600

    Chat page progress

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -6,7 +6,8 @@ import { useState } from "react"
 const Chat = () => {
 
     const handleEmojiClick = e => {
-        console.log(e)
+        setText((prev) => prev + e.emoji);
+        setOpen
     }
     const [open, setOpen] = useState(false)
     const [text, setText] = useState('')
@@ -28,17 +29,57 @@ const Chat = () => {
                     <img src="./info.png" alt="" />
                 </div>
             </div>
-            <div className="center"></div>
+            <div className="center">
+                <div className="message">
+                    <img src="./avatar.png" alt="" />
+                    <div className="texts">
+                        <p>Random message here</p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="message">
+                    <img src="./avatar.png" alt="" />
+                    <div className="texts">
+                        <p>Random message here</p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="message own">
+                    <div className="texts">
+                        <p>Random message here</p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="message">
+                    <img src="./avatar.png" alt="" />
+                    <div className="texts">
+                        <p>Random message here</p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+                <div className="message own">
+                    <div className="texts">
+                        <p>Random message here</p>
+                        <span>1 min ago</span>
+                    </div>
+                </div>
+            </div>
             <div className="bottom">
                 <div className="icons">
                     <img src="./img.png" alt="" />
                     <img src="./camera.png" alt="" />
                     <img src="./mic.png" alt="" />
                 </div>
-                <input type="text" placeholder="Type a message" />
+                <input type="text" placeholder="Type a message" onChange={e => setText(e.target.value)}
+                    value={text}
+                />
                 <div className="emoji">
-                    <img src="./emoji.png" alt="" onClick={(prev) => !prev} />
-                    <EmojiPicker open={open} onEmojiClick={handleEmojiClick} />
+                    <img src="./emoji.png" alt=""
+                        onClick={() => setOpen((prev) => !prev)}
+                    />
+                    <div className="picker">
+                        <EmojiPicker open={open} onEmojiClick={handleEmojiClick} />
+                    </div>
                 </div>
                 <button className="sendButton">Send</button>
             </div>
--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -3,7 +3,41 @@ import "./detail.css"
 
 const Detail = () => {
     return (
-        <div className='detail'>Detail</div>
+        <div className='detail'>
+            <div className="user">
+                <img src="./avatar.png" alt="" />
+                <h2>Evan Spencer</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At non modi possimus consequuntur expedita distinctio amet officiis ab, temporibus repellat quia quas eaque, et maxime necessitatibus nemo similique incidunt error.</p>
+            </div>
+            <div className="info">
+                <div className="option">
+                    <div className="title">
+                        <span>Chat Settings</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>Chat Settings</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+                <div className="option">
+                    <div className="title">
+                        <span>Privacy & help</span>
+                        <img src="./arrowUp.png" alt="" />
+                    </div>
+                </div>
+                <div className="photos">
+                    <div className="photoItems">
+                        <span>photo_2023_2.png</span>
+                        <img src="./arrowUp.png" alt="" />
+
+                    </div>
+                    <img src="./download.png" alt="" />
+                </div>
+            </div>
+        </div>
     )
 }
 
